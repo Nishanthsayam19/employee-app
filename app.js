9a63a65d70805b9d215ef11d11349fc4b94b1a7c
Your Name <yourmail@example.com>
2026-08-06 17:20:53 +0530

    new version 4 added EmpName

--- app.js
+++ app.js
@@ -1,45 +1,4 @@
-let employees = [];
-let id = 1;
-
 function addEmployee() {
+	alert("Button is working!");
 
-	let name = document.getElementById("empNmae").value;
-
-	if(name==""){
-		alert("Please Enter Employee Nmae");
-		return;
-	}
-
-	employees.push({
-		id;id,
-		name:name
-	});
-	
-	displayEmployees();
-
-	id++;
-
-	documents.getElementById("empNmae").value="";
-
-}
-
-function displayEmployees(){
-
-	let table=document.getElementById("employeeTable");
-
-	table.innerHTML="";
-
-	employees.forEach(function(emp){
-
-		table.innerHTML += `
-		<tr>
-
-		  <td>${emp.id}</td>
-		  <td>${emp.name}</td>
-
-	</tr>
-	
-	`;
-
-	});
 }
