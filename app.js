79ce6a1a3b008555f071d0d24f017f5a1fda793b
Your Name <yourmail@example.com>
2026-08-06 17:09:21 +0530

    new version 4 added EmpName

--- app.js
+++ app.js
@@ -1,3 +1,45 @@
-</> JavaScript
+let employees = [];
+let id = 1;
 
-alert("Employee Application Loaded");
+function addEmployee() {
+
+	let name = document.getElementById("empNmae").value;
+
+	if(name==""){
+		alert("Please Enter Employee Nmae");
+		return;
+	}
+
+	employees.push({
+		id;id,
+		name:name
+	});
+	
+	displayEmployees();
+
+	id++;
+
+	documents.getElementById("empNmae").value="";
+
+}
+
+function displayEmployees(){
+
+	let table=document.getElementById("employeeTable");
+
+	table.innerHTML="";
+
+	employees.forEach(function(emp){
+
+		table.innerHTML += `
+		<tr>
+
+		  <td>${emp.id}</td>
+		  <td>${emp.name}</td>
+
+	</tr>
+	
+	`;
+
+	});
+}
