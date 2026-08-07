4ea6d80551fdd584b6854f4988abd86a134bd060
Your Name <yourmail@example.com>
2026-08-07 16:50:13 +0530

    Added employee functionality

--- app.js
+++ app.js
@@ -1,4 +1,43 @@
+let employee = [];
+let id = 1;
+
 function addEmployee() {
-	alert("Button is working!");
+
+	let name = document.getElementById("empName").value;
+
+	if (name === "") {
+		alert("Please enter employee name");
+		return;
+
+
+]
+
+employees.push({
+	id: id,
+	name: name
+
+});
+
+		displayEmployees();
+
+		id++;
+
+		documnet.getElementById("empName").value = "";
 
 }
+
+function displayEmployees() {
+	let table = document.getElementByid("employeeTable");
+
+	table.innerHTML = "";
+
+	employees.forEach(function(emp) {
+		table.innerHTML += `
+		<tr>
+		<td>${emp.id}</td>
+		<td>${emp.name}</td>
+		</tr>
+		`;
+	});
+}
+
